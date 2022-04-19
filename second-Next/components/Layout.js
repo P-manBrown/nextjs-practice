fa5b3e2ec2c3e0a18b5efb603c43f2238059b2b1
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-04-19 13:48:08 +0900

    [add] second-next layout.module.css

--- second-Next/components/Layout.js
+++ second-Next/components/Layout.js
@@ -1,22 +1,23 @@
 import Head from "next/head";
+import styles from "./layout.module.css"
 
 const name = "P-man Brown"
 export const siteTitle = "Next.js blog"
 
 function Layout({ chirdren }) {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
-      <header>
+      <header className={styles.header}>
         <img src="/images/pb-logo.jpg" />
         <h1>{name}</h1>
       </header>
       <main>
         {chirdren}
       </main>
-    </>
+    </div>
   );
 }
 

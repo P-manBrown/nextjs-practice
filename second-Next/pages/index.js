32134c3895c1b728bb7d55da07ec73c67a159b82
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-04-19 14:20:44 +0900

    [add] second-next index.js

--- second-Next/pages/index.js
+++ second-Next/pages/index.js
@@ -7,7 +7,26 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <Layout>
-      <a href=''>next.jsです</a>
+      <section>
+        <p>未経験からエンジニア転職を目指して日々学習に励んでいます。</p>
+      </section>
+      <section>
+        <h2>学習の記録</h2>
+        <div>
+          <article>
+            <Link href="">
+              <img src="/images/thumbnail01.jpg" />
+            </Link>
+            <Link href="">
+              <a>【SQL】SQL入門</a>
+            </Link>
+            <br />
+            <small>
+              2021年12月22日
+            </small>
+          </article>
+        </div>
+      </section>
     </Layout>
   )
 }

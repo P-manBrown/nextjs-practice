8b44bf081ab0d28f260d7cf7462f9d9a523f6743
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-04-27 12:34:14 +0900

    feat: add detail-task page(ISR)
    タスク詳細ページをISRで作成

--- nextjs-blog-todos/src/components/Task.js
+++ nextjs-blog-todos/src/components/Task.js
@@ -1,11 +1,15 @@
+import Link from 'next/link'
+
 export default function Task({ task }) {
   return (
     <div>
       <span>{task.id}</span>
       {' : '}
-      <span className="cursor-pointer border-b border-gray-500 text-white hover:bg-gray-600">
-        {task.title}
-      </span>
+      <Link href={`/tasks/${task.id}`}>
+        <span className="cursor-pointer border-b border-gray-500 text-white hover:bg-gray-600">
+          {task.title}
+        </span>
+      </Link>
     </div>
   )
 }

0476a6e9f19a26c0c09388a649ae1739eb34f899
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-04-27 15:32:55 +0900

    feat: create and delete tasks
    タスクの追加機能と削除機能を追加

--- nextjs-blog-todos/src/pages/task-page.js
+++ nextjs-blog-todos/src/pages/task-page.js
@@ -4,6 +4,8 @@ import useSWR from 'swr'
 
 import Layout from '../components/Layout'
 import Task from '../components/Task'
+import TaskFrom from '../components/TaskForm'
+import StateContextProvider from '../context/StateContext'
 import { getAllTasksData } from '../lib/tasks'
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
@@ -20,33 +22,36 @@ export default function TaskPage({ staticFilteredTasks }) {
     mutate()
   }, [])
   return (
-    <Layout title="Task page">
-      <ul>
-        {filteredTasks &&
-          filteredTasks.map((task) => (
-            <Task key={task.id} task={task} taskDeleted={mutate} />
-          ))}
-      </ul>
-      <Link href="/main-page">
-        <div className="mt-12 flex cursor-pointer">
-          <svg
-            className="mr-3 h-6 w-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
-            />
-          </svg>
-          <span>Back to main page</span>
-        </div>
-      </Link>
-    </Layout>
+    <StateContextProvider>
+      <Layout title="Task page">
+        <TaskFrom taskCreated={mutate} />
+        <ul>
+          {filteredTasks &&
+            filteredTasks.map((task) => (
+              <Task key={task.id} task={task} taskDeleted={mutate} />
+            ))}
+        </ul>
+        <Link href="/main-page">
+          <div className="mt-12 flex cursor-pointer">
+            <svg
+              className="mr-3 h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+              />
+            </svg>
+            <span>Back to main page</span>
+          </div>
+        </Link>
+      </Layout>
+    </StateContextProvider>
   )
 }
 

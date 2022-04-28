fe95323bfd1c8fb17abc51740fdb31789e457056
P-manBrowm <tachikawa.yuya@outlook.jp>
2022-04-28 10:13:51 +0900

    build: running create-next-app

---
+++ nextjs-testing/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center font-mono">
+      <div>Hello Nextjs</div>
+    </div>
+  )
+}
+
+export default Home

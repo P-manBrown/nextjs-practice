66b4537ba166287e2bef5f0a2512c6113c7ad99f
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2022-05-06 15:12:32 +0900

    finish: 2.10

--- next-ec-npm/next.config.js
+++ next-ec-npm/next.config.js
@@ -1,9 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    // styledComponentsの有効化
-    styledComponents: true,
+  compiler: (() => {
+    let compilerConfig = {
+      styledComponents: true,
+    }
+
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        reactRemoveProperties: { properties: ['^data-testid$'] },
+      }
+    }
+
+    return compilerConfig
+  })(),
+  async rewrites() {
+    return [
+      {
+        // ex. /api/proxy
+        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:match*`,
+        // ex. http://localhost:8000
+        destination: `${process.env.API_BASE_URL}/:match*`,
+      },
+    ]
   },
 }
 

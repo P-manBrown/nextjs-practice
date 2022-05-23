086e48e336c5efb633fdc15f8e3053901c377145
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2022-05-23 18:42:53 +0900

    finish(tailwind-css): 4.23

--- tailwind-css/tailwind.config.js
+++ tailwind-css/tailwind.config.js
@@ -3,14 +3,17 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        primary: {
-          100: "#ebf8ff",
-          300: "#90cdf4",
-          500: "#4299e1"
-        }
-      }
+      // colors: {
+      //   primary: {
+      //     100: "#ebf8ff",
+      //     300: "#90cdf4",
+      //     500: "#4299e1"
+      //   }
+      // },
     },
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    }
   },
   plugins: [],
 }

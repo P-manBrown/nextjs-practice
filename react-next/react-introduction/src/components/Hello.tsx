1bab991fbeabea97c238a73f13432ce64d10530a
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2022-06-15 17:36:01 +0900

    finish(react-next): 3.2.1

--- react-next/react-introduction/src/components/Hello.tsx
+++ react-next/react-introduction/src/components/Hello.tsx
@@ -1,14 +1,21 @@
+import Name from "./Name"
+
 const Hello = () => {
   const onClick = () => {
     alert('hello')
   }
 
   const text = 'Hello, React'
+  const name = 'React'
 
   return (
-    <div onClick={onClick}>
-      {text}
-    </div>
+    <>
+      <div onClick={onClick}>
+        {text}
+      </div>
+      <span>こんにちは、{name}さん</span>
+      <Name />
+    </>
   )
 }
 

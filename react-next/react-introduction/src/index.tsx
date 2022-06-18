de3473c39948d1cf11e50fbf59a922116d3b3444
P-manBrown <80106172+P-manBrown@users.noreply.github.com>
2022-06-18 19:34:35 +0900

    finish(react-next): 3.5.2

--- react-next/react-introduction/src/index.tsx
+++ react-next/react-introduction/src/index.tsx
@@ -8,6 +8,8 @@ import Parent from './components/ContainerSample';
 import Page from './components/ContextSample';
 import Counter from './components/Counter';
 import CounterReducer from './components/CounterReducer';
+import { ParentMemo } from './components/ParentMemo';
+import { CountCallback } from './components/CountCallback';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -20,6 +22,8 @@ root.render(
     <Page />
     <Counter initialValue={100} />
     <CounterReducer initialValue={10} />
+    <ParentMemo />
+    <CountCallback />
   </React.StrictMode>
 );
 
